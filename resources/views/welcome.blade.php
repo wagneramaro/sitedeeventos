619d456390f795c7a086a76cd87abb65ae29d6e6
Wagner Amaro <wagneramaro.dev@gmail.com>
2023-09-15 02:06:55 -0300

    Aula 21 - Busca no Laravel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,14 +4,19 @@
 
        <div id="search-container" class="col-md-12">
         <h1>Busque um evento</h1>
-        <form action="">
+        <form action="/" method="get">
             <input type="text" id="search" name="search" class="form-control" placeholder="Pesquisar..." >
         </form>
        </div>
 
        <div id="events-container" class="col-md-12">
+        @if ($search)
+        <h2>Buscando por: {{$search}}</h2>
+        @else
         <h2>Próximos Eventos</h2>
         <p class="subtitle">Veja os eventos dos próximos dias.</p>
+        @endif
+
         <div id="cards-container" class="row">
             @foreach ($events as $event)
 
@@ -34,7 +39,9 @@
 
             @endforeach
 
-            @if (count($events) == 0)
+            @if (count($events) == 0 && $search)
+            <p>Não foi possível encontrar nenhum evento com  {{$search}}! <a href="/">Ver todos.</a></p>
+            @elseif(count($events) ==0)
                 <p>Não há eventos disponíveis.</p>
             @endif
 
