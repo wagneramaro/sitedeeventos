883355fca93b35401759aa52b023e32bd4e96967
Wagner Amaro <wagneramaro.dev@gmail.com>
2023-09-19 07:15:41 -0300

    Update dashboard.blade.php

--- resources/views/events/dashboard.blade.php
+++ resources/views/events/dashboard.blade.php
@@ -5,7 +5,7 @@
 <div class="col-md-10 offset-md-1 dashboard-title-container">
     <h1>Meus Eventos</h1>
 </div>
-<div class="col-md-10 offset-md-1 dashboard-eventos-container">
+<div class="col-md-10 offset-md-1 dashboard-events-container">
     @if (count($events)>0)
     <table class="table">
         <thead>
