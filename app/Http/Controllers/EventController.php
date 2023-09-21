1457e4c30324e5da984864b598d1b91ba9373372
Wagner Amaro <wagneramaro.dev@gmail.com>
2023-09-21 08:16:01 -0300

    Criando update de evntos

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -87,5 +87,35 @@ class EventController extends Controller
 
 
     }
+
+    public function edit($id){
+
+        $event = Event::findOrFail($id);
+
+        return view('events.edit', ['event'=> $event]);
+
+    }
+
+    public function update(Request $request){
+
+        $data = $request->all();
+
+         //Image upload
+         if($request->hasFile('image') && $request->file('image')->isValid()){
+            $requestImage = $request->image;
+
+            $extension = $requestImage->extension();
+
+            $imageName = md5($requestImage->getClientOriginalName() . strtotime('now')). '.'. $extension;
+
+            $request->image->move(public_path('img/events'), $imageName);
+
+            $data['image'] = $imageName;
+        }
+
+        Event::findOrFail($request->id)->update($data);
+
+        return redirect('/dashboard')->with('msg', 'Evento editado com sucesso!');
+    }
 }
 
